7d4d862004b3ea8f29081e92edac8fc43fa789ec
Rémi Palancher <remi-externe.palancher@edf.fr>
2018-10-08 11:03:16 +0200

    Overwrite 9990-netbase.sh to restrict ethdevices
    The commit adds the 9990-netbase.sh to overwrite the one provided in
    live-boot package. It makes the script iterate over the list of ethernet
    devices provided in ethdevices= cmdline argument to generate initial
    /etc/network/interfaces file instead of _ALL_ the physical devices.
    
    This way, it avoids running a DHCP client, and eventually waiting for
    them to timeout, on all the ethernet physical devices of the nodes.
    
    Here is the diff with original netbase.sh script from live-boot
    1:20170112:
    
    --- a/boot/9990-netbase.sh
    +++ b/boot/9990-netbase.sh
    @@ -94,13 +94,15 @@
     			method="manual"
     		fi
    
    -		# iterate the physical interfaces and add them to the interfaces list and also add when ethdevice= called on cmdline
     		if [ "${method}" != dhcp ] || ([ ! -x /root/usr/sbin/NetworkManager ] && [ ! -x /root/usr/sbin/wicd ]) || [ ! -z "${ETHDEVICE}" ]
     		then
    -			for interface in /sys/class/net/eth* /sys/class/net/ath* /sys/class/net/wlan* /sys/class/net/en*
    -			do
    -				[ -e ${interface} ] || continue
    -				i="$(basename ${interface})"
    +			# if ethdevice is not set, iterate the physical interfaces and add them to the interfaces list
    +			if [ -z ${ETHDEVICE} ]
    +			then
    +				for interface in /sys/class/net/eth* /sys/class/net/ath* /sys/class/net/wlan* /sys/class/net/en*
    +				do
    +					[ -e ${interface} ] || continue
    +					i="$(basename ${interface})"
    
     cat >> "${IFFILE}" << EOF
     allow-hotplug ${i}
    @@ -108,7 +110,18 @@
    
     EOF
    
    -			done
    +				done
    +			else
    +				# when ethdevice= called on cmdline split it and iterate over it
    +				for device in $(echo $ETHDEVICE | sed 's/,/ /g')
    +				do
    +cat >> "${IFFILE}" << EOF
    +allow-hotplug ${device}
    +iface ${device} inet ${method}
    +
    +EOF
    +				done
    +			fi
     		fi
    
     		if [ ! -f "${DNSFILE}" ] || [ -z "$(cat ${DNSFILE})" ]

--- scripts/remove-orig.sh
+++ scripts/remove-orig.sh
@@ -17,7 +17,7 @@ OPTS="-q"
 
 # Remove old scripts from initrd 
 LDIR="/lib/live/boot"
-ORIG="9990-mount-http.sh.orig 9990-cmdline-old.orig 9990-overlay.sh.orig"
+ORIG="9990-mount-http.sh.orig 9990-cmdline-old.orig 9990-overlay.sh.orig 9990-netbase.sh.orig"
 for file in ${ORIG}
 do
 	trgfile=${LDIR}/${file}
